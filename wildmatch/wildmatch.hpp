6e6a5f5543b63c85f112a939d662cedfc787f256
David Aguilar <davvid@gmail.com>
2016-05-22 19:17:40 -0700

    wildmatch: add a C++ example
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- wildmatch/wildmatch.hpp
+++ wildmatch/wildmatch.hpp
@@ -4,6 +4,8 @@
 
 namespace wild {
 
+/* Flags */
+constexpr int FNMATCH = 0x00; /* No flags, fnmatch behavior. */
 constexpr int NOESCAPE = 0x01; /* Disable backslash escaping. */
 constexpr int PATHNAME = 0x02; /* Slash must be matched by slash. */
 constexpr int PERIOD = 0x04; /* Period must be matched by period. */
